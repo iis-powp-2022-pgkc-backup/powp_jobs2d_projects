6c7844dc6d8174893c4e1c502b6e5abf7a9b89fd
S-T-A-L-K-R <name.generate@gmail.com>
2022-05-04 11:09:03 +0200

    Transformation POC

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dApp.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dApp.java
@@ -11,6 +11,7 @@ import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.command.gui.CommandManagerWindow;
 import edu.kis.powp.jobs2d.command.gui.CommandManagerWindowCommandChangeObserver;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDriverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.TransformDriverAdapter;
 import edu.kis.powp.jobs2d.events.SelectLoadSecretCommandOptionListener;
 import edu.kis.powp.jobs2d.events.SelectRunCurrentCommandOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigure2OptionListener;
@@ -66,6 +67,10 @@ public class TestJobs2dApp {
 
 		driver = new LineDriverAdapter(drawerController, LineFactory.getSpecialLine(), "special");
 		DriverFeature.addDriver("Special line Simulator", driver);
+		DriverFeature.updateDriverInfo();
+
+		driver = new TransformDriverAdapter(drawerController, LineFactory.getBasicLine(), "transformed");
+		DriverFeature.addDriver("Move line to the right Simulator", driver);
 		DriverFeature.updateDriverInfo();
 	}
 
