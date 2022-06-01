12c68b368577d7ebcd2357157b115c1931ad4c03
S-T-A-L-K-R <name.generate@gmail.com>
2022-06-01 11:25:11 +0200

    nno to inaczej...

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TransformDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/TransformDriverAdapter.java
@@ -3,20 +3,37 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.powp.jobs2d.Job2dDriver;
 
 
-public class TransformDriverAdapter extends LineDriverAdapter {
+public class TransformDriverAdapter implements Job2dDriver {
+
+	protected ILine line;
+	protected int startX = 0, startY = 0;
+	protected String name;
+
+	protected DrawPanelController drawController;
+	protected LineDriverAdapter lineDriverAdapter;
 
 	public TransformDriverAdapter(DrawPanelController drawController, ILine line, String name) {
-		super(drawController, line, name);
+		super();
+		this.drawController = drawController;
+		this.line = line;
+		this.name = name;
+		this.lineDriverAdapter = new LineDriverAdapter(this.drawController, this.line, this.name);
+	}
+	@Override
+	public void operateTo(int x, int y) {
+		// line.setStartCoordinates(this.startX+100, this.startY);
+		// this.setPosition(x, y);
+		// line.setEndCoordinates(x+100, y);
+
+		// drawController.drawLine(line);
 	}
 
 	@Override
-	public void operateTo(int x, int y) {
-		line.setStartCoordinates(this.startX+100, this.startY);
-		this.setPosition(x, y);
-		line.setEndCoordinates(x+100, y);
-
-		drawController.drawLine(line);
+	public void setPosition(int x, int y) {
+		this.startX = x;
+		this.startY = y;
 	}
 }
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Transformation.java
@@ -0,0 +1,7 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+public interface Transformation {
+    // public int parameter;
+    
+    public void transform(int x, int y);
+}
